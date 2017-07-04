704d0396d361ec7ded0e2a8ee215243aa74521ac
alec aivazis <alec@aivazis.com>
2017-07-04 04:23:35 -0700

    added missing file

--- Assets/Local/Engine/Board/WARActorTable.cs
+++ Assets/Local/Engine/Board/WARActorTable.cs
@@ -15,12 +15,8 @@ namespace WAR.Engine.Board {
     [RequireComponent(typeof(WARControlBoard))]
 	public class WARActorTable : MonoBehaviour {
 
-        public void initialize(UIPlane plane, GRID_TYPE cellType){
-            // find a random planet from the list of prefabs
-            GameObject planet = planetPrefabs[UnityEngine.Random.Range(0, planetPrefabs.Count)];
-
-            // attach the planet to the table
-            planet = GameObject.Instantiate(planet, transform);
+		public void initialize(UIPlane plane, GRID_TYPE cellType){
+			
         }
 	}
 }
