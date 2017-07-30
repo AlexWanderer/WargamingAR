8f8c0ce5283722e4370553b3a3e34553cf23c20e
alec aivazis <alec@aivazis.com>
2017-07-30 00:47:58 -0700

    damagability is now part of WARUnit

--- Assets/Engine/Units/Scripts/WARUnit.cs
+++ Assets/Engine/Units/Scripts/WARUnit.cs
@@ -8,5 +8,19 @@ namespace WAR.Units {
 	public abstract class WARUnit : WARMovableObject {
 		// the id of the player that owns this unit
 		public int owner;
+		
+		[SerializeField]
+		public int maxHealth;
+		private int currentHealth;
+		
+		public void Start() {
+		// start off at max health
+			currentHealth = maxHealth;
+		}
+		
+		public void takeDamage(int damage) {
+		// for now, just decrement the health
+			maxHealth -= damage;
+		}
 	}
 }
