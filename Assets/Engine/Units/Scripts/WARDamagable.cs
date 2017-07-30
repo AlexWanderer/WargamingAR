9ec681586527204956b10a6b120d1b14dfd95d9f
alec aivazis <alec@aivazis.com>
2017-07-30 00:34:13 -0700

    added damagable component

---
+++ Assets/Engine/Units/Scripts/WARDamagable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WARDamagable : MonoBehaviour {
+	
+	[SerializeField]
+	public int maxHealth;
+	private int currentHealth;
+	
+	public void Start() {
+		// start off at max health
+		currentHealth = maxHealth;
+	}
+	
+	public void takeDamage(int damage) {
+		// for now, just decrement the health
+		maxHealth -= damage;
+	}
+}
